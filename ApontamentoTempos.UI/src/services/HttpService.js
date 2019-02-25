9765b7851d652bb5de123952087ccd8564654c29
rafael.persch <rafaelpersch@hotmail.com>
2019-02-24 22:32:28 -0300

    tratar erro

--- ApontamentoTempos.UI/src/services/HttpService.js
+++ ApontamentoTempos.UI/src/services/HttpService.js
@@ -7,7 +7,7 @@ export default class HttpService {
 
     post(url, obj, anonymous){
 
-        return new Promise((resolve, reject)=> {
+        return new Promise((resolve)=> {
             let myHeaders = {};
 
             if (!anonymous){
@@ -22,29 +22,31 @@ export default class HttpService {
                             this.sessionService.set(res.body);
                             resolve(this.post(url, obj, anonymous));
                         }else{
-                            reject({ sucesso: false, retorno: "401 Unauthorized", status: 401 });
+                            resolve({ sucesso: false, retorno: "401 Unauthorized", status: 401 });
                         }            
                     }, err => {
-                        reject({ sucesso: false, retorno: "401 Unauthorized: " + err.body, status: 401 });
+                        resolve({ sucesso: false, retorno: "401 Unauthorized: " + err, status: 401 });
                     });
                 }
                 else if (res.status == 200){
                     resolve({ sucesso: true, retorno: res.body, status: res.status });
                 }else{
-                    reject({ sucesso: false, retorno: res.body, status: res.status });
+                    resolve({ sucesso: false, retorno: res.body, status: res.status });
                 }            
             }, err => {
-                reject({ sucesso: false, retorno: err.body, status: err.status });
+                console.log("Erro POST: " + err);
+                resolve({ sucesso: false, retorno: "Erro", status: 0 });
             })
             .catch((err) => {
-                reject({ sucesso: false, retorno: "catch" + err, status: 500 });
+                console.log("Erro POST catch: " + err);
+                resolve({ sucesso: false, retorno: "Erro", status: 0 });
             });
         });
     }
     
     get(url, anonymous){
 
-        return new Promise((resolve, reject)=> {
+        return new Promise((resolve)=> {
             let myHeaders = {};
 
             if (!anonymous){
@@ -59,36 +61,38 @@ export default class HttpService {
                             this.sessionService.set(res.body);
                             resolve(this.get(url, anonymous));
                         }else{
-                            reject({ sucesso: false, retorno: "401 Unauthorized", status: 401 });
+                            resolve({ sucesso: false, retorno: "401 Unauthorized", status: 401 });
                         }            
                     }, err => {
-                        reject({ sucesso: false, retorno: "401 Unauthorized: " + err.body, status: 401 });
+                        resolve({ sucesso: false, retorno: "401 Unauthorized: " + err, status: 401 });
                     });
 
                 }
                 else if (res.status == 200){
                     resolve({ sucesso: true, retorno: res.body, status: res.status });
                 }else{
-                    reject({ sucesso: false, retorno: res.body, status: res.status });
+                    resolve({ sucesso: false, retorno: res.body, status: res.status });
                 }
             }, err => {
-                reject({ sucesso: false, retorno: err.body, status: err.status });
+                console.log("Erro GET: " + err);
+                resolve({ sucesso: false, retorno: "Erro", status: 0 });
             })
             .catch((err) => {
-                reject({ sucesso: false, retorno: "catch" + err, status: 500 });
+                console.log("Erro GET catch: " + err);
+                resolve({ sucesso: false, retorno: "Erro", status: 0 });
             });             
         });
     }
 
     put(url, id, obj, anonymous){
 
-        return new Promise((resolve, reject)=> {
+        return new Promise((resolve)=> {
             let myHeaders = {};
 
             if (!anonymous){
                 myHeaders = { 'Authorization': 'Bearer ' + this.sessionService.get().accessToken };
             }
-    
+
             this.resource.put(url + "/" + id, obj, { headers: myHeaders }).then(res => {
                 if (res.status == 401) {
 
@@ -97,29 +101,31 @@ export default class HttpService {
                             this.sessionService.set(res.body);
                             resolve(this.put(url, id, obj, anonymous));
                         }else{
-                            reject({ sucesso: false, retorno: "401 Unauthorized", status: 401 });
+                            resolve({ sucesso: false, retorno: "401 Unauthorized", status: 401 });
                         }            
                     }, err => {
-                        reject({ sucesso: false, retorno: "401 Unauthorized: " + err.body, status: 401 });
+                        resolve({ sucesso: false, retorno: "401 Unauthorized: " + err, status: 401 });
                     });
                 }
                 else if (res.status == 200){
                     resolve({ sucesso: true, retorno: res.body, status: res.status });
                 }else{
-                    reject({ sucesso: false, retorno: res.body, status: res.status });
+                    resolve({ sucesso: false, retorno: res.body, status: res.status });
                 }
             }, err => {
-                reject({ sucesso: false, retorno: err.body, status: err.status });
+                console.log("Erro PUT: " + err);
+                resolve({ sucesso: false, retorno: "Erro", status: 0 });
             })
             .catch((err) => {
-                reject({ sucesso: false, retorno: "catch" + err, status: 500 });
+                console.log("Erro PUT catch: " + err);
+                resolve({ sucesso: false, retorno: "Erro", status: 0 });
             });            
         });
     }
 
     delete(url, id, anonymous){
 
-        return new Promise((resolve, reject)=> {
+        return new Promise((resolve)=> {
             let myHeaders = {};
 
             if (!anonymous){
@@ -134,23 +140,25 @@ export default class HttpService {
                             this.sessionService.set(res.body);
                             resolve(this.delte(url, id, anonymous));
                         }else{
-                            reject({ sucesso: false, retorno: "401 Unauthorized", status: 401 });
+                            resolve({ sucesso: false, retorno: "401 Unauthorized", status: 401 });
                         }            
                     }, err => {
-                        reject({ sucesso: false, retorno: "401 Unauthorized: " + err.body, status: 401 });
+                        resolve({ sucesso: false, retorno: "401 Unauthorized: " + err, status: 401 });
                     });
 
                 }
                 else if (res.status == 200){
                     resolve({ sucesso: true, retorno: res.body, status: res.status });
                 }else{
-                    reject({ sucesso: false, retorno: res.body, status: res.status });
+                    resolve({ sucesso: false, retorno: res.body, status: res.status });
                 }
             }, err => {
-                reject({ sucesso: false, retorno: err.body, status: err.status });
+                console.log("Erro DELETE: " + err);
+                resolve({ sucesso: false, retorno: "Erro", status: 0 });
             })
             .catch((err) => {
-                reject({ sucesso: false, retorno: "catch" + err, status: 500 });
+                console.log("Erro DELETE catch: " + err);
+                resolve({ sucesso: false, retorno: "Erro", status: 0 });
             });             
         });
     }
