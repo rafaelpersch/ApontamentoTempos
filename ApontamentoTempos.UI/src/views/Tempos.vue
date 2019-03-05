d5b1edae000f1231dfe02315a8a11dbdb4db5606
rafael.persch <rafaelpersch@hotmail.com>
2019-03-04 21:38:29 -0300

    tempos

--- ApontamentoTempos.UI/src/views/Tempos.vue
+++ ApontamentoTempos.UI/src/views/Tempos.vue
@@ -76,7 +76,7 @@ export default {
           var data = this.getResponseData(resp.registros); 
           var data2 = [];
           for (var key in data) {
-            data2.push({data:data[key].datastring, projeto:data[key].projeto.nome, issue:data[key].issue, id: data[key].id});
+            data2.push({data:data[key].dataString, projeto:data[key].projeto.nome, issue:data[key].issue, id: data[key].id});
           }          
 
           return { data: data2, count: resp.count };
